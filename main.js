75d39ff9277c168331b49492d8911fa515c8c4c2
Ethan Daniel <ethandaniel@berkeley.edu>
2020-01-12 19:10:56 -0800

    have indicator move when buttons are selected

--- main.js
+++ main.js
@@ -1,13 +1,35 @@
+let descriptionButtons = document.getElementsByClassName("work-btn");
+	descriptionButtons.array.forEach(element => {
+		element.onclick = toggleButtons(element.id);
+	});
+
 function toggleButtons(index) {
 	let descriptionDivs = document.getElementsByClassName("work-description");
 	for (let i = 0; i < descriptionDivs.length; i++) {
 		let description = descriptionDivs[i];
 		if (i == index) {
 			description.style.display = "block";
-			console.log("set " + i + " to visible with index " + index);
 		} else {
 			description.style.display = "none";
-			console.log("set " + i + " to hidden with index " + index);
 		}
 	}
+
+	let indicator = document.getElementsByClassName("indicator")[0];
+	console.log(indicator);
+	switch (index) {
+		case 0:
+			indicator.className = "indicator zeroth-elem";
+			break;
+		case 1:
+			indicator.className = "indicator first-elem";
+			break;
+		case 2:
+			indicator.className = "indicator second-elem";
+			break;
+		case 3:
+			indicator.className = "indicator third-elem";
+			break;
+	}
+
+	
 }
